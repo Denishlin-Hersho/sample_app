9a16bc3be190b23032c00d9c3426e8efa3d1af9a
Denishlin-Hersho <hershod@gmail.com>
2024-08-01 10:49:44 +0530

    some changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  class << self
     # Returns the hash digest of the given string.
     def digest(string)
       cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -21,13 +20,12 @@ class User < ApplicationRecord
     def new_token
       SecureRandom.urlsafe_base64
     end
-  end
 
   # Remembers a user in the database for use in persistent sessions.
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
-    remember_digest
+    # remember_digest
   end
   # Returns a session token to prevent session hijacking.
   # We reuse the remember digest for convenience.
@@ -73,9 +71,12 @@ class User < ApplicationRecord
   def downcase_email
     self.email = email.downcase
   end
-  # Creates and assigns the activation token and digest.
+    # Creates and assigns the activation token and digest.
   def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(activation_token)
   end
 end
+
+
+
