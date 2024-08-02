0dfcf4c8884be0a6d7e0bbfad7803408fe1520db
Denishlin-Hersho <hershod@gmail.com>
2024-08-02 11:26:57 +0530

    new changes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -51,12 +51,13 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
-    end
+  end
 
-    test "password should have a minimum length" do
-      @user.password = @user.password_confirmation = "a" * 5
-      assert_not @user.valid?
-    end
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
   end
