6a81cc94ab536fab671e240da6041db8cde1e757
Denishlin-Hersho <hershod@gmail.com>
2024-08-05 17:08:18 +0530

    new changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'password_resets/new'
-  get 'password_resets/edit'
-   root 'static_pages#home'
-   get '/help',   to: 'static_pages#help'
-   get '/about',  to: 'static_pages#about'
-   get '/contact',to: 'static_pages#contact'
-   get '/signup', to: 'users#new'
-   get '/login',  to: 'sessions#new'
-
-   post '/login', to: 'sessions#create'
-
-  delete 'logout',to: 'sessions#destroy'
-
-=======
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
@@ -22,31 +7,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> user-microposts
-  resources :users
-  resources :account_activations, only: [:edit]
-  resources :password_resets, only: [:new, :create, :edit, :update]
-
-=======
-
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
->>>>>>> user-microposts
-=======
-  resources :users
-  resources :account_activations, only: [:edit]
-<<<<<<< HEAD
-  resources :password_resets,
-            only: [:new, :create, :edit, :update]
-  resources :microposts,
-            only: [:create, :destroy]
->>>>>>> user-microposts
-=======
-  resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :microposts, only: [:create, :destroy]
->>>>>>> user-microposts
 end
